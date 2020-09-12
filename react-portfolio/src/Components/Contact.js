bf067304ded16af78eaed0ac807d82709c90d4c1
Rubin <rubinradhakrishnan@gmail.com>
2020-09-12 21:21:20 +0530

    Change the mailto: params

--- react-portfolio/src/Components/Contact.js
+++ react-portfolio/src/Components/Contact.js
@@ -13,7 +13,9 @@ const Contact = ({ data }) => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+    window.open(
+      `mailto:rubinradhakrishnan@gmail.com?subject=${subject}&body=${name}: ${message}`
+    );
   };
 
   return (
